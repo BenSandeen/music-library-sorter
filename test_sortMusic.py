d8d23e1276869db953750860267ae704c4e6ed1e
BenSandeen <root@SPARTA.localdomain>
2016-09-10 02:25:32 -0500

    Created Song class and music sorting class.  Also began test suite

--- test_sortMusic.py
+++ test_sortMusic.py
@@ -1,37 +1,53 @@
 #### To be run using pytest testing library ####
 
-import sortMusic as SM
+import sortMusicFiles as SM
+import song as Song
+import sys
 
 class TestClass:
     """pytest automatically finds and runs these tests.  The class is effectively a means
        of organizing the tests in logical compartments"""
 
     def setup(self):
-        self.song = SM.Song()
+        self.song = Song.Song()
 
     def test_one(self):
         assert self.song.getTitle() == "No title info"
-
-    def test_two(self):
         assert self.song.getArtist() == "No artist info"
-
-    def test_three(self):
         assert self.song.getAlbum() == "No album info"
-
-    def test_five(self):
         assert self.song.getAlbumArtist() == "No albumartist info"
-
-    def test_six(self):
         assert self.song.getDate() == "No date info"
-
-    def test_seven(self):
         assert self.song.getTrackNumber() == "No tracknumber info"
-
-    def test_eight(self):
         assert self.song.getTrackTotal() == "No tracktotal info"
 
+    def setup(self):
+        """TODO: may need to use mocking or something to create an object.
+           Alternatively, you may just need to not initialize the self.musicLocation
+           variable when creating a SortMusicFiles object"""
+        sys.stdin.write("C://")
+        self.musicSorter = SM.SortMusicFiles()
+        sys.stdin.write("C://")
+
+    def test_two(self):
+        pass
+
+    def test_three(self):
+        pass
+
+    def test_four(self):
+        pass
+
+    def test_five(self):
+        pass
+
+    def test_six(self):
+        pass
+
     def test_seven(self):
+        pass
+
+    def test_eight(self):
         self.song.setArtist("Queen")
         assert 'Queen' in self.song.getArtist()
 
-    # def test_three(self):
+    # def test_nine(self):
